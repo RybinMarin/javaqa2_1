f0186c67aa776f935490fd9c7e4a6ce4e00cfc5f
Marina Rybina <marina.rybina1310@gmail.com>
2023-05-07 18:33:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int amount = 13676;
+       int step = 20;
+
+       int mile = amount / step;
+
+        System.out.println("Итого миль за покупку:" + mile);
+    }
+}
