d941ded1ee431398bdc95895519f9cc331b56a4b
Marina Rybina <marina.rybina1310@gmail.com>
2023-05-12 18:41:25 +0300

    Code commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-       int amount = 13676;
-       int step = 20;
+        int amount = 13676;
+        int step = 20;
 
-       int mile = amount / step;
+        int mile = amount / step;
 
         System.out.println("Итого миль за покупку:" + mile);
     }
